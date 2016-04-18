17441daa12f7dd03ad1b38c88ae529409760d0cb
wuhailei <wuhailei1989@gmail.com>
2016-04-18 22:09:09 +0800

    异常处理

--- socket/gevent_server.py
+++ socket/gevent_server.py
@@ -13,35 +13,40 @@ db = client.bdp
 
 def handle(socket, address):
     """ 处理请求 """
-    data = socket.recv(1024)
+    try:
+        data = socket.recv(1024)
 
-    key = binascii.b2a_hex(data[0:16]).upper()
+        key = binascii.b2a_hex(data[0:16]).upper()
 
-    body = data[16:]
+        body = data[16:]
 
-    # RN解密
-    rnc = RNC(key)
-    data = rnc.decrypt(body)
+        # RN解密
+        rnc = RNC(key)
+        data = rnc.decrypt(body)
 
-    data = json.loads(data)
+        data = json.loads(data)
+        
+        usr = data['usr']
+        udid = data['udid']
+        
+        print 'request from %s' % usr
+
+        user = db.users.find_one({'udid': udid})
+
+        if user and user.get('status'):
+            status = 1 
+        else:
+            status = 0
+
+        # 2、查询mongo
+        response = {'udid': udid, 'status': status}
+        cipher_text = rnc.encrypt(json.dumps(response))
+        socket.send(cipher_text)
+    except Exception, e:
+    	pass
+    finally:
+        socket.close()
     
-    usr = data['usr']
-    udid = data['udid']
-    
-    print 'request from %s' % usr
-
-    user = db.users.find_one({'udid': udid})
-
-    if user and user.get('status'):
-        status = 1 
-    else:
-        status = 0
-
-    # 2、查询mongo
-    response = {'udid': udid, 'status': status}
-    cipher_text = rnc.encrypt(json.dumps(response))
-    socket.send(cipher_text)
-    socket.close()
 
 server_address = ('0.0.0.0', 6666)
 server = StreamServer(server_address, handle)
